9ccaa718f9e5f5a1b67fa029af72915a31573bd9
Omar Barazanji <ozanji345@gmail.com>
2021-10-19 16:46:41 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Parkinson's detector using a public dataset containing speech data from 
+patients and healthy participants. The model will be trained using 
+Gradient Boosting via the open source XGBoost package.
+
+authors:
+Omar Barazanji
+-sign here-
+
+date: 10/19/2021
+
+dataset and references:
+1) https://www.kaggle.com/vikasukani/parkinsons-disease-data-set
+2) 
+'''
+
