e0c7181a6dedf7ac6d27df024d7c15754fb5f006
Omar Barazanji <ozanji345@gmail.com>
2021-11-14 13:55:02 -0600

    added learning curves and cleaned up code a bit

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ Parkinson's detector using a public dataset containing speech data from
 patients and healthy participants. The model will be trained using 
 Gradient Boosting via the open source XGBoost package.
 
-authors:
-Omar Barazanji
--sign here-
+author: Omar Barazanji
 
 date: 10/19/2021
 
@@ -13,10 +11,12 @@ dataset and references:
 1) https://www.kaggle.com/vikasukani/parkinsons-disease-data-set
 2) https://www.kaggle.com/vikasukani/detecting-parkinson-s-disease-machine-learning
 3) https://stackoverflow.com/questions/35572000/how-can-i-plot-a-confusion-matrix
+4) https://machinelearningmastery.com/tune-xgboost-performance-with-learning-curves/
 '''
 
 # Import helper modules 
 import os
+import time
 import numpy as np
 import pandas as pd
 import seaborn as sn
@@ -72,25 +72,54 @@ class Model:
         Train the model using XGBClassifier with training data.
         Print the accuracy of the model by testing predicted labels to actual.
         '''
-        self.model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
-        self.model.fit(self.x_train, self.y_train)
-        self.y_pred = self.model.predict(self.x_test)
-        accuracy = accuracy_score(self.y_test,self.y_pred)*100
-        print("Accuracy: %d" % accuracy)
+        evalset = [(self.x_train, self.y_train), (self.x_test,self.y_test)]
 
+        self.model = XGBClassifier(use_label_encoder=False, eval_metric='logloss', n_estimators=500)
+        self.model.fit(self.x_train, self.y_train,eval_set=evalset)
+        self.y_pred = self.model.predict(self.x_test)
+        self.accuracy = accuracy_score(self.y_test,self.y_pred)*100
+        print("Accuracy: %d" % self.accuracy)
+
+        loss = self.model.evals_result()
+
+        # plot learning curves
+        fig1 = plt.figure()
+        plt.plot(loss['validation_0']['logloss'], label='train')
+        plt.plot(loss['validation_1']['logloss'], label='test')
+        plt.xlabel('Loss')
+        plt.ylabel('Iterations')
+        plt.title('XGBoost Learning Curves')
+        plt.legend()
+        plt.show()
+
+        fig2 = plt.figure()
         c_matrix = confusion_matrix(self.y_test, self.y_pred)
         df = pd.DataFrame(c_matrix, range(len(c_matrix)), range(len(c_matrix)))
         sn.set(font_scale=1.2)
         sn.heatmap(df, annot=True, annot_kws={"size": 14})
+        plt.title('XGBoost Confusion Matrix')
         plt.show()
 
 if __name__ == "__main__":
 
-    # Construct the model class
-    parkinsons = Model()
+    num_runs = 1
+    results = []
+    for run in range(num_runs):
+        # Construct the model class
+        parkinsons = Model()
 
-    # Pre-process data
-    parkinsons.pre_process()
+        # Pre-process data
+        parkinsons.pre_process()
 
-    # Train the model
-    parkinsons.train()
+        t0 = time.time()
+
+        # Train the model
+        parkinsons.train()
+
+        time_len = time.time() - t0
+
+        results_arr = [run, parkinsons.accuracy, time_len]
+
+        results.append(results_arr)
+    
+    np.savetxt('xgboost_results.csv', results, delimiter=',')
